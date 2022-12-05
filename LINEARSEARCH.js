a41fa2df9b8e2a403e817920610c9c94d3a96e16
Ivan090900 <118599345+Ivan090900@users.noreply.github.com>
2022-12-05 21:14:57 +0200

    Add files via upload

---
+++ LINEARSEARCH.js
@@ -0,0 +1,18 @@
+const mass =[
+    { userNAME: 'ANA',  age: 7 },
+    { userNAME: 'INNA', age:21 },
+    { userNAME: 'IVAN', age: 17},
+    { userNAME: 'STEPAN', age: 49,}
+]
+
+function linerSerarch (array, mass){
+    for(let i =0 ; i < arr.length; i++){
+        if(array[i]. index === mass){
+            console.log (arr[i]);
+            return i; 
+        }
+    }
+    return -1
+}
+
+console.log (linearSearch(mass, 19));
