9d6bc8d119321142fb62e4b62932a6fa96cfbc09
Ivan090900 <118599345+Ivan090900@users.noreply.github.com>
2022-12-09 12:59:26 +0200

    Add files via upload

--- 18.js
+++
@@ -1,9 +0,0 @@
-const numbers = [7,10,15,23,29,28];
-function linerSerach (array, num){
-    for (let i = 0 ; i < array.length; i++ ){
-        if (array [i] === num ){
-            return i;
-        }
-    }
-    return -1;
-}console.log (linearSearch(number, 18))
